7a5671899d34c18c1282a081e080bbc3359a27e0
paulkokos <paulkokos@yahoo.gr>
2020-10-05 20:08:31 +0300

    Builder Pattern

---
+++ src/[03]Builder Pattern/[09]Gamma Categorization/readme.md
@@ -0,0 +1,11 @@
+#### Creational patterns </br>
+* Deal with the creation (construction) of objects
+* Explicit (constructor) VS implisit (DI,reflection)
+* Wholesale (single statement) VS piecewise (step-by-step)
+#### Structural Patterns
+* Concerned with the structure (e.g. class members)
+* Many patterns are wrappers that mimic the underlying class interface
+* Stress the importance of good API design
+#### Behavioral Patterns
+* They are all difference; No central theme
+* 
---
+++ src/[03]Builder Pattern/[12]Builder/[12]Builder.cpp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <tuple>
+#include <sstream>
+#include <memory>
+using namespace std;
+int main() {
+    auto text = "Hello";
+    string output;
+    output += "<p>";
+    output += text;
+    output += "</p>";
+
+    string words[] = {"Hello","World"};
+    ostringstream oss;
+    oss << "<ul>";
+    for (auto w:words) {
+        oss << "<li>" << w<< "</li>";
+    }
+    oss << "</ul>";
+    cout << oss.str()<< endl;
+    return 0;
+}
