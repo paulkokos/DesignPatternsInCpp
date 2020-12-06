a2e8a5fadb232d1833af60b3326cac2a7e95cf88
paulkokos <paulkokos@yahoo.gr>
2020-12-06 23:30:26 +0200

    [005]Liskov Substitution Principle

--- src/[002]SOLID Design Principles/[004]Open-Closed Principle/[004]Open-Closed Principle.cpp
+++ src/[002]SOLID Design Principles/[004]Open-Closed Principle/[004]Open-Closed Principle.cpp
@@ -5,24 +5,125 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include "include/Color.hpp"
-#include "include/Size.hpp"
-#include "include/Product.hpp"
-#include "include/Filter.hpp"
-#include "include/AndSpecification.hpp"
-#include "include/Specification.hpp"
-#include "include/BetterFilter.hpp"
-#include "include/ColorSpecification.hpp"
-#include "include/SizeSpecification.hpp"
-
-
 using namespace std;
 
+enum class Color { red, green, blue };
+enum class Size { small, medium, large };
+struct Product{string name;Color color;Size size;};
+
+struct ProductFilter
+{
+    typedef vector<Product*> Items;
+
+    Items by_color(Items items, const Color color)
+    {
+        Items result;
+        for (auto& i : items)
+            if (i->color == color)
+                result.push_back(i);
+        return result;
+    }
+
+    Items by_size(Items items, const Size size)
+    {
+        Items result;
+        for (auto& i : items)
+            if (i->size == size)
+                result.push_back(i);
+        return result;
+    }
+
+    Items by_size_and_color(Items items, const Size size, const Color color)
+    {
+        Items result;
+        for (auto& i : items)
+            if (i->size == size && i->color == color)
+                result.push_back(i);
+        return result;
+    }
+};
+
+template <typename T> struct AndSpecification;
+
+template <typename T> struct Specification
+{
+    virtual ~Specification() = default;
+    virtual bool is_satisfied(T* item) const = 0;
+
+    // new: breaks OCP if you add it post-hoc
+    /*AndSpecification<T> operator&&(Specification<T>&& other)
+    {
+      return AndSpecification<T>(*this, other);
+    }*/
+};
+
+// new:
 template <typename T> AndSpecification<T> operator&&
         (const Specification<T>& first, const Specification<T>& second)
 {
     return { first, second };
 }
+
+template <typename T> struct Filter
+{
+    virtual vector<T*> filter(vector<T*> items,
+                              Specification<T>& spec) = 0;
+};
+
+struct BetterFilter : Filter<Product>
+{
+    vector<Product*> filter(vector<Product*> items,
+                            Specification<Product> &spec) override
+    {
+        vector<Product*> result;
+        for (auto& p : items)
+            if (spec.is_satisfied(p))
+                result.push_back(p);
+        return result;
+    }
+};
+
+struct ColorSpecification : Specification<Product>
+{
+    Color color;
+
+    ColorSpecification(Color color) : color(color) {}
+
+    bool is_satisfied(Product *item) const override {
+        return item->color == color;
+    }
+};
+
+struct SizeSpecification : Specification<Product>
+{
+    Size size;
+
+    explicit SizeSpecification(const Size size)
+            : size{ size }
+    {
+    }
+
+
+    bool is_satisfied(Product* item) const override {
+        return item->size == size;
+    }
+};
+
+template <typename T> struct AndSpecification : Specification<T>
+{
+    const Specification<T>& first;
+    const Specification<T>& second;
+
+    AndSpecification(const Specification<T>& first, const Specification<T>& second)
+            : first(first), second(second) {}
+
+    bool is_satisfied(T *item) const override {
+        return first.is_satisfied(item) && second.is_satisfied(item);
+    }
+};
+
+// new:
+
 int main()
 {
     Product apple{"Apple", Color::green, Size::small};
@@ -52,6 +153,6 @@ int main()
     // auto spec2 = SizeSpecification{Size::large} &&
     //              ColorSpecification{Color::blue};
 
-    cin.get();
+    getchar();
     return 0;
 }
